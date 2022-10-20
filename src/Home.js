ff53b267d0da126433cf1299fab9e3a2fcdcfeba
Yotam Granada <yotam.granada@gmail.com>
2022-10-20 14:15:22 +0300

    add data base

---
+++ src/Home.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([null]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+
+    fetch('http://localhost:8000/blogs')
+    .then(res => res.json())
+    .then(data => {
+      console.log("data", data);
+      setBlogs(data);
+    })
+  },[]);
+
+  return (
+    <div className="home">
+      { blogs && <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete} />}
+    </div>
+  );
+};
+
+export default Home;
